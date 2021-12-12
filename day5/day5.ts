3ab07695d7b9889e847d1fcfce34a9c8b46e0dca
Lukas Jenks <lukasjenks@gmail.com>
2021-12-12 11:02:46 -0700

    Finished day 5

--- day5/day5.ts
+++ day5/day5.ts
@@ -1,11 +1,11 @@
-import { stdin, stdout } from 'process';
+import { stdout } from 'process';
 import { line } from './line';
 
 /*
     Following function is purposefully verbose for readability. 
 */
-function getPuzzleInput (fileName : string) : [any[], number, number] {
-	const fs = require('fs')
+function getPuzzleInput (fileName : string, questionNumber: number) : [any[], number, number] {
+	const fs = require('fs');
     let coordLines = [];
     let maxX = 0;
     let maxY = 0;
@@ -13,29 +13,33 @@ function getPuzzleInput (fileName : string) : [any[], number, number] {
 	try {
 		const data : string = fs.readFileSync(fileName, 'utf8')
 		const lines = data.split("\n");
-        let insertIndex = 0;
         for (let lineNum=0; lineNum < lines.length; lineNum++)  {
             let coords : string[] = lines[lineNum].split(" -> ");
-            let x1 : number = parseInt(coords[0].split(",")[0].trim());
-            let y1 : number = parseInt(coords[0].split(",")[1].trim());
-            let x2 : number = parseInt(coords[1].split(",")[0].trim());
-            let y2 : number = parseInt(coords[1].split(",")[1].trim());
+            let x1 : number = parseInt(coords[0].split(",")[0]);
+            let y1 : number = parseInt(coords[0].split(",")[1]);
+            let x2 : number = parseInt(coords[1].split(",")[0]);
+            let y2 : number = parseInt(coords[1].split(",")[1]);
 
-            if (x1 == x2 || y1 == y2) {
+            if (questionNumber === 1) {
+                if (x1 == x2 || y1 == y2) {
+                    coordLines.push({x1: x1, y1: y1, x2: x2, y2: y2});
+                    maxX = Math.max(maxX, x1, x2);
+                    maxY = Math.max(maxY, y1, y2);
+                }
+            } else if (questionNumber === 2) {
                 coordLines.push({x1: x1, y1: y1, x2: x2, y2: y2});
                 maxX = Math.max(maxX, x1, x2);
                 maxY = Math.max(maxY, y1, y2);
             }
         }
-        //console.log(coordLines);
 	} catch (err) {
-	    console.error(err)
+	    console.error(err);
+        return err;
 	}
-    //console.log(coordLines);
     return [coordLines, maxX, maxY];
 }
 
-function getSolutionOne(coordLines, maxX : number, maxY : number) : void {
+function getSolution(coordLines, maxX : number, maxY : number, partNumber: number) : void {
     let grid = [];
     for (let y = 0; y <= maxY+1; y++) {
         grid[y] = [];
@@ -45,19 +49,25 @@ function getSolutionOne(coordLines, maxX : number, maxY : number) : void {
     }
     for (let lineDef of coordLines) {
         let lineObj = new line(lineDef.x1, lineDef.y1, lineDef.x2, lineDef.y2);
-        let coordsToAdd = lineObj.getLineCoords();
-        console.log(lineDef)
-        console.log(coordsToAdd);
+        let coordsToAdd : number[][];
+        if (partNumber === 1) {
+            coordsToAdd = lineObj.getStraightLineCoords();
+        } else if (partNumber === 2) {
+            coordsToAdd = lineObj.getAllLineCoords();
+        }
         for (let coord of coordsToAdd) {
-            // y, x
             grid[coord[1]][coord[0]]++;
         }
     }
-    printGridMap(grid, maxX, maxY);
+    console.log("Part " + partNumber + " Solution: " + countOverlaps(grid, maxX, maxY));
 }
 
+// Function purely for visualizing grid for testing
 function printGridMap(grid: any[], maxX : number, maxY : number) {
-    stdout.write("x  0 1 2 3 4 5 6 7 8 9\n");
+    stdout.write("x  ");
+    for (let x = 0; x <= maxX; x++) {
+        stdout.write(x + " ");
+    }
     stdout.write("----------------------\n");
     for (let y = 0; y <= maxY; y++) {
         stdout.write(y.toString() + "| ");
@@ -68,10 +78,23 @@ function printGridMap(grid: any[], maxX : number, maxY : number) {
     }
 }
 
+function countOverlaps(grid: number[][], maxX : number, maxY : number) : number {
+    let overlaps = 0;
+    for (let y = 0; y <= maxY; y++) {
+        for (let x = 0; x <= maxX; x++) {
+            if (grid[y][x] > 1) {
+                overlaps++;
+            }
+        }
+    }
+    return overlaps;
+}
+
 const main = () => {
-    const [coordLines, maxX, maxY] = getPuzzleInput('./day5EX.txt');
-    //console.log(coordLines, maxX, maxY);
-    getSolutionOne(coordLines, maxX, maxY);
+    let [coordLines, maxX, maxY] = getPuzzleInput("./day5PuzzleInput.txt", 1);
+    getSolution(coordLines, maxX, maxY, 1);
+    [coordLines, maxX, maxY] = getPuzzleInput("./day5PuzzleInput.txt", 2);
+    getSolution(coordLines, maxX, maxY, 2);
 }
 
 main();
@@ -80,3 +103,5 @@ main();
 
 // 1814 wrong
 // 1784 wrong
+
+// 20424 too low for part two
--- day5/line.ts
+++ day5/line.ts
@@ -12,21 +12,57 @@ class line {
     }
 
     // Returns 2D array of x and y coordinates
-    public getLineCoords(this: line): number[][] {
+    public getStraightLineCoords(this: line): number[][] {
         let coords: number[][] = [];
         if (this.x1 === this.x2 && this.y1 === this.y2) {
             coords.push([this.x1, this.y1]);
         } else if (this.x1 === this.x2) {
-            // Determine if x is going up or down
+            // Determine if y is going up or down for length of line
             let increment : number = this.y1 < this.y2 ? 1 : -1;
             for (let y = this.y1; y !== this.y2; y += increment) {
                 coords.push([this.x1, y]);
             }
             coords.push([this.x2, this.y2]);
         } else if (this.y1 === this.y2) {
+            // Determine if x is going up or down for length of line
             let increment : number = this.x1 < this.x2 ? 1 : -1;
             for (let x = this.x1; x !== this.x2; x += increment) {
                 coords.push([x, this.y1]);
+            }
+            coords.push([this.x2, this.y2]);
+        }
+        return coords
+    }
+
+    public getAllLineCoords(this: line): number[][] {
+        let coords: number[][] = [];
+        // Line is single point coordinate
+        if (this.x1 === this.x2 && this.y1 === this.y2) {
+            coords.push([this.x1, this.y1]);
+        // Line is vertical
+        } else if (this.x1 === this.x2) {
+            let increment : number = this.y1 < this.y2 ? 1 : -1;
+            for (let y = this.y1; y !== this.y2; y += increment) {
+                coords.push([this.x1, y]);
+            }
+            coords.push([this.x2, this.y2]);
+        // Line is horizontal
+        } else if (this.y1 === this.y2) {
+            let increment : number = this.x1 < this.x2 ? 1 : -1;
+            for (let x = this.x1; x !== this.x2; x += increment) {
+                coords.push([x, this.y1]);
+            }
+            coords.push([this.x2, this.y2]);
+        // Line is diagonal
+        } else {
+            let xIncrement : number = this.x1 < this.x2 ? 1 : -1;
+            let yIncrement : number = this.y1 < this.y2 ? 1 : -1;
+            let x = this.x1;
+            let y = this.y1;
+            while (x !== this.x2 || y !== this.y2) {
+                coords.push([x, y]);
+                x += xIncrement;
+                y += yIncrement;
             }
             coords.push([this.x2, this.y2]);
         }
